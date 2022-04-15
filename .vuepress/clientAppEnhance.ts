8863dcc58c99f7f1c0968cf8b7135d28620aefd4
jeffjing <zgayjjf@qq.com>
2022-04-15 10:24:10 +0800

    fix: anchor bug

--- .vuepress/clientAppEnhance.ts
+++ .vuepress/clientAppEnhance.ts
@@ -1,6 +1,31 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 import Redirect from './components/Redirect.vue'
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+/**
+ * Wait for an element with specific selector to show.
+ * @param selector
+ * @param timeout
+ */
+async function waitFor(selector: string, timeout = 2000) {
+  let $el
+  let startAt = Date.now()
+  let now = Date.now()
+
+  while (!$el && now - startAt < timeout) {
+    $el = document.querySelector(selector)
+
+    if ($el) break
+
+    await sleep(100)
+
+    now = Date.now()
+  }
+
+  return $el
+}
+
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   app.component(Redirect.name, Redirect)
 
@@ -25,4 +50,21 @@ export default defineClientAppEnhance(({ app, router, siteData }) => {
       next()
     }
   })
+
+  // For some unknown reason, the anchor(hash) didn't take effect, so we have to handle it manually
+  // Hope it can be fixed one day.
+  router.afterEach((async (to, from, failure) => {
+    if (!from.name && to.name && to.hash) {
+      const $el = await waitFor(to.hash)
+      $el?.scrollIntoView()
+
+      await sleep(500)
+
+      // it is very strange that the element will be rebuilt, thus they are not the same element.
+      const $el2 = await waitFor(to.hash)
+      if ($el !== $el2) {
+        $el2?.scrollIntoView()
+      }
+    }
+  }))
 })
